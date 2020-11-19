8d082c868f14b95785b90c62cf470923528ebf8f
ariel taylor <ariel.taylorr@gmail.com>
2020-11-19 11:21:53 -0800

    Add uniq specs

--- lib/tdd.rb
+++ lib/tdd.rb
@@ -1,6 +1,11 @@
 class Array
 
     def uniq
+        result = []
+        self.each do |ele|
+            result << ele if !result.include?(ele)
+        end
+        result
     end
 
 end
--- spec/tdd_spec.rb
+++ spec/tdd_spec.rb
@@ -2,10 +2,18 @@ require "tdd"
 require "rspec"
 
 RSpec.describe Array do
+    let(:arr) {Array.new([])}
     describe "#uniq" do
-        it "returns new array" 
+        it "does not modify the original array" do 
+            arr = [1,1]
+            arr.uniq
+            expect(arr).to eq([1,1]) 
+        end 
 
-        it "removes duplicate elements"
+        it "removes duplicate elements" do
+            arr = [1,1]
+            expect(arr.uniq).to eq([1]) 
+        end
         
         it "new arr contains unique elements in order they appeared"
 
