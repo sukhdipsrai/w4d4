d98bc78430940537c4593dac0a3eb58e4655f615
Sukhdip Rai <sukhdipsrai@gmail.com>
2020-11-19 17:21:59 -0500

    finished TDD

--- lib/hanoi.rb
+++ lib/hanoi.rb
@@ -17,4 +17,39 @@ class Hanoi
         @tower_array[-1] == (1..@disks).to_a.reverse
     end
 
+    def get_move
+        until win?
+            self.render
+            p " Give me first tower"
+            t1 = gets.chomp.to_i
+            p " Give me second tower"
+            t2 = gets.chomp.to_i
+
+            if valid_move?(t1,t2)
+                move(t1,t2)
+            else
+                p "Not Valid Moves"
+            end
+        end
+    end
+
+    def valid_move?(t1,t2)
+        return false if t1 > towers && t1 <= 0
+        return false if t2 > towers && t2 <= 0
+        return false if t2 == t1
+        return false if tower_array[t1].empty? 
+        true
+    end
+
+    def render 
+        flipped = tower_array.dup
+        # pad it to look pretty, then transpose
+        flipped.each do |cols|
+            p cols
+        end     
+    end
+
 end
+
+h = Hanoi.new
+h.get_move
